dc10deaaf0aa9c3d1367e5838895abcde133eaaf
ayodeji <ayodejiakintobi1@gmail.com>
2024-10-16 23:07:44 +0100

    update: images

--- frontend/src/constants/Navbar.jsx
+++ frontend/src/constants/Navbar.jsx
@@ -3,22 +3,19 @@ import LOGO from "../../public/LOGO.png";
 
 const Navbar = () => {
   return (
-    <section className="flex">
+    <section className="flex justify-around mt-4 bg-[url('/hero.png')] bg-center bg-cover min-h-100px">
       <div className="flex space-x-2 items-center">
         <img src={LOGO} className="w-[25px] h-[25px]" />
-        <span className="text-[18px] font-semi-bold leading-tight]">
+        <span className="text-[20px] font-semi-bold leading-tight]">
           CrowdRaise
         </span>
       </div>
-
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Charity</li>
-          <li>Disaster</li>
-          <li>Event</li>
-        </ul>
-      </div>
+      <ul className="flex justify-center space-x-4 text-[18px] font-medium cursor-pointer">
+        <li>Home</li>
+        <li>Charity</li>
+        <li>Disaster</li>
+        <li>Event</li>
+      </ul>
     </section>
   );
 };
