a6209772c7f0beafd70d30b45d5bdb8d53761c76
ayodeji <ayodejiakintobi1@gmail.com>
2024-10-27 22:58:29 +0100

    UPDATE: SCRIPT CROWDFUNDING

---
+++ backend/script/DeployCrowdFunding.s.sol
@@ -0,0 +1,17 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol"
+import {CrowdFunding} from "../src/CrowdFunding.sol";
+
+contract DeployCrowdFunding is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        CrowdFunding crowdfunding = new CrowdFunding();
+
+        console.log("Contract deployed at:", address(crowdfunding))
+        vm.stopBroadcast();
+    }
+}
