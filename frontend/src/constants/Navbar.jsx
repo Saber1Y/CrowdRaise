e02e8d7234d73f779c49ebdd3662a9e615fbc93e
ayodeji <ayodejiakintobi1@gmail.com>
2024-11-28 00:12:47 +0100

    UPDATE: HAMBURGER MENU CONVERTED

--- frontend/src/constants/Navbar.jsx
+++ frontend/src/constants/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className="absolute top-0 left-0 w-full flex justify-between items-center p-6 z-20 bg-white text-black shadow-md">
-      
+      {/* Logo */}
       <div className="flex items-center space-x-2">
         <img src={LOGO} className="w-[25px] h-[25px]" alt="Logo" />
         <span className="text-[20px] font-semibold leading-tight">
@@ -18,7 +18,7 @@ const Navbar = () => {
         </span>
       </div>
 
-      
+      {/* Desktop Menu */}
       <ul className="hidden md:flex justify-center space-x-5 text-[18px] font-medium cursor-pointer">
         <li>
           <Link to="/">Home</Link>
@@ -35,15 +35,22 @@ const Navbar = () => {
         className="md:hidden flex items-center focus:outline-none"
         onClick={() => setIsMenuOpen(!isMenuOpen)}
       >
-        <span className="sr-only">Open Menu</span>
-        <div className="space-y-2">
-          <span className="block w-6 h-[2px] bg-black"></span>
-          <span className="block w-6 h-[2px] bg-black"></span>
-          <span className="block w-6 h-[2px] bg-black"></span>
-        </div>
+        <span className="sr-only">Toggle Menu</span>
+        {isMenuOpen ? (
+          <div className="space-y-1">
+            <span className="block w-6 h-[2px] bg-black rotate-45 translate-y-1"></span>
+            <span className="block w-6 h-[2px] bg-black -rotate-45 -translate-y-1"></span>
+          </div>
+        ) : (
+          <div className="space-y-2">
+            <span className="block w-6 h-[2px] bg-black"></span>
+            <span className="block w-6 h-[2px] bg-black"></span>
+            <span className="block w-6 h-[2px] bg-black"></span>
+          </div>
+        )}
       </button>
 
-     
+      {/* Mobile Menu */}
       {isMenuOpen && (
         <div className="absolute top-[70px] left-0 w-full bg-white shadow-lg p-5 md:hidden">
           <ul className="flex flex-col items-center space-y-4 text-[18px] font-medium cursor-pointer">
@@ -66,6 +73,7 @@ const Navbar = () => {
         </div>
       )}
 
+  
       <div className="hidden md:block">
         <ConnectButton />
       </div>
